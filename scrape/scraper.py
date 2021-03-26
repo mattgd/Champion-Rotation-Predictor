e01d60a6a222decb1688b1b6caae66e45203c1bf
Anna Chernikov <annaleezac@gmail.com>
2021-03-25 21:50:19 -0700

    reworked scraper

--- scrape/scraper.py
+++ scrape/scraper.py
@@ -4,73 +4,73 @@
 import json
 import requests
 from bs4 import BeautifulSoup
+from pygrok import Grok
+from datetime import date
 
-# get secret key
-execfile('keys.py')
+def monthToNum(mon):
+    return {
+            'January' : 1,
+            'February' : 2,
+            'March' : 3,
+            'April' : 4,
+            'May' : 5,
+            'June' : 6,
+            'July' : 7,
+            'August' : 8,
+            'September' : 9,
+            'October' : 10,
+            'November' : 11,
+            'December' : 12
+    }[mon]
 
-urls = ['http://leagueoflegends.wikia.com/wiki/Champion_Rotation_Schedule_(Pre-Season_One)', 'http://leagueoflegends.wikia.com/wiki/Champion_Rotation_Schedule_(Season_One)', 'http://leagueoflegends.wikia.com/wiki/Champion_Rotation_Schedule_(Pre-Season_Two)', 'http://leagueoflegends.wikia.com/wiki/Champion_Rotation_Schedule_(Season_Two)', 'http://leagueoflegends.wikia.com/wiki/Champion_Rotation_Schedule_(Pre-Season_Three)', 'http://leagueoflegends.wikia.com/wiki/Champion_Rotation_Schedule_(Season_Three)', 'http://leagueoflegends.wikia.com/wiki/Champion_Rotation_Schedule_(Pre-Season_Four)', 'http://leagueoflegends.wikia.com/wiki/Champion_Rotation_Schedule_(Season_Four)', 'http://leagueoflegends.wikia.com/wiki/Champion_Rotation_Schedule_(Pre-Season_Five)', 'http://leagueoflegends.wikia.com/wiki/Champion_Rotation_Schedule_(Season_Five)']
+#Function to transform dictionary to python date
+def toDate(dic):
+	return date(int(dic['year']),monthToNum(dic['month']),int(dic['day']))
 
-for url in urls:
+
+#Wiki
+wiki='https://leagueoflegends.fandom.com'
+#Champion rotation archive link
+url = 'https://leagueoflegends.fandom.com/wiki/Champion_rotation_archive'
+
+#### Get the URL for each season's wiki page ####
+# Grab HTML
+soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+# Access list of past champion rotations for each season
+htmlList = soup.find('div',class_='WikiaArticle').find_all('ul')[1].find_all('li')
+hrefList = []
+for l in htmlList:
+	href=str(l.find('a')['href'])
+	season=str(href.split('/')[-1])
+	hrefList.append(wiki + href+"#"+"Pre-"+season)
+	hrefList.append(wiki + href+"#"+season)
+
+
+
+#### Get champion and date information for each wiki ####
+weekNum = 0
+date_pattern = '%{MONTHDAY:day} %{MONTH:month} %{YEAR:year}'
+grok = Grok(date_pattern)
+
+for url in hrefList:
+	#Grab HTML
 	soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-
-	anchors = soup.find(id='mw-content-text').findAll('table')[3].find('td').findAll('a')
-	anchor = anchors[len(anchors) - 1]
-	champion = anchor.findAll(text=True)[0]
-
-	tables = soup.find(id='mw-content-text').findChildren('table', recursive=False) # find all direct table children
-
-	data = {}
-
-	tableNumber = 0
-
-	for index, row in enumerate(tables):
-		tables = row.findAll('table')
-
-		for j, table in enumerate(tables):
-			if j % 2 == 0:
-				data[tableNumber] = {}
-				data[tableNumber]['champions'] = []
-
-				# get date
-				text = table.findAll(text=True)
-				year = text[4]
-
-				if ',' not in year:
-					year = ',' + year # add comma if it isn't present
-
-				date = text[2] + year
-
-				split = date.split(',')
-				date = split[0] + ',' + split[1][0:5] # cut off any extra bits after year (this will work for 4 digit years)
-
-				data[tableNumber]['date'] = date
-			else:
-				# get champions
-				tds = table.findAll('td')
-
-				for k, content in enumerate(tds):
-					if k % 2 == 0:
-						# get champion name
-						anchors = content.findAll('a')
-						anchor = anchors[len(anchors) - 1]
-						text = anchor.findAll(text=True)
-
-						if len(text) == 0:
-							champion = anchor['title']
-						else:
-							champion = text[0]
-
-						data[tableNumber]['champions'].append(champion)
-
-				tableNumber += 1
-						
-
-        # print out data that script returns
-	# print(json.dumps(data, indent = 4))
-
-        # convert data to json and send to server
-	data_json = json.dumps(data)
-        payload = { 'data': data_json, 'key': insert_key }
-        print("sending post")
-	r = requests.post('http://localhost/scrape/insert.php', data=payload)
-	print(r.text)
+	#Get the table for each week of the pre-season/season
+	tables = soup.find_all('table',class_='wikitable')
+	#Go through each week for the pre-season/season
+	for table in tables:
+		# Increment the week number
+		weekNum += 1
+		#Get the dates for that week
+		dates = table.find('p').getText().replace(u'\xa0', ' ').replace(u'\n', ' ').split(" - ")
+		start_date = toDate(grok.match(dates[0]))
+		end_date = toDate(grok.match(dates[1]))
+		#get the list of champions for that week
+		champions = []
+		for champ in table.find_all("div", {"data-game": "lol"}):
+			champions.append(champ.find('a')['title'])
+		#Insert into DB
+		print(weekNum, end="  ")
+		print(start_date, end="  ")
+		print(end_date, end="  ")
+		print(champions)
