c524561f0e86c04253d14452b5655c53c3f84f95
mattgd <mattdzwonczyk@gmail.com>
2021-03-27 23:16:52 -0700

    Add API for getting rotations for specific champion.

--- backend/app/views/rotations.py
+++ backend/app/views/rotations.py
@@ -1,8 +1,18 @@
 from flask import Blueprint, jsonify
 
+from ..models.champion import Champion
 from ..models.rotation import Rotation
 
 rotations_api = Blueprint('rotations', __name__, url_prefix='/api/rotations')
+
+
+@rotations_api.route('/', methods=['GET'])
+def get_rotations():
+    """
+    Returns a JSON representation of all free champions for the given rotation week.
+    """
+    rotations = Rotation.query.all()
+    return jsonify(rotations)
 
 
 @rotations_api.route('/<int:week>', methods=['GET'])
@@ -13,3 +23,13 @@ def get_rotation_week(week: int):
     rotations = Rotation.query.filter(Rotation.week_number == week)
     champions = [rotation.champions.to_dict() for rotation in rotations]
     return jsonify(champions)
+
+
+@rotations_api.route('/<string:champion_name>', methods=['GET'])
+def get_rotation_by_champion(champion_name: str):
+    """
+    Returns a JSON representation of all free rotations for a given champion.
+    """
+    rotations = Rotation.query.join(Rotation.champions).filter(Champion.name == champion_name).all()
+    rotations = [rotation.to_dict() for rotation in rotations]
+    return jsonify(rotations)
